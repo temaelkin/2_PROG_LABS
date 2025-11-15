b1cef1f906089e91a1f831d79f749faec0782668
temaelkin <elkin.ao@ya.ru>
2025-11-15 21:53:44 +0300

    Fix logic & refactor

--- LAB3/bin_tree.py
+++ LAB3/bin_tree.py
@@ -1,6 +1,9 @@
 import json
 
-def gen_bin_tree(height: int = 4, root: int | float = 3) -> dict | None:
+def gen_bin_tree(height: int = 4,
+                 root: int | float = 3, 
+                 left_branch = lambda x: x + 2, 
+                 right_branch = lambda x: x * 3) -> dict | None:
     """
     Recursively generate a binary tree as a nested dictionary.
 
@@ -17,21 +20,24 @@ def gen_bin_tree(height: int = 4, root: int | float = 3) -> dict | None:
     ----------
     height : The height of the tree (must be >= 0). Defaults to 4.
     root : The value stored in the root node. Defaults to 3.
+    left_branch : Lambda for computing left child's value. Defaults to x + 2.
+    right_branch : Lambda for computing right child's value. Defaults to x * 3.
 
     Returns
     -------
     A nested dictionary representing the binary tree,
     or None if the height is 0.
     """
-    if height <= 0:
-        return None
-    if height == 1:
-        return {"value": root, "left": None, "right": None}
 
-    return {
-        "value": root,
-        "left": gen_bin_tree(height-1, root+2),
-        "right": gen_bin_tree(height-1, root*3)
-    }
+    if height < 0:
+        return None
+    elif height == 0:
+        return root
+    else:
+        return {
+            "value": root,
+            "left": gen_bin_tree(height-1, left_branch(root), left_branch, right_branch),
+            "right": gen_bin_tree(height-1, right_branch(root), left_branch, right_branch)
+        }
 
 print(json.dumps(gen_bin_tree(), indent=4))
